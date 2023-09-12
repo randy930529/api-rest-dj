c36173ca1276112d75c031221d247899b4f6dd61
randy930529 <randy930529@gmail.com>
2023-09-12 13:29:33 -0400

    set userScheme to email required

--- src/auth/schema/userSchema.ts
+++ src/auth/schema/userSchema.ts
@@ -4,11 +4,13 @@ var decimal =
   /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,30}$/;
 
 const userScheme = Joi.object({
-  email: Joi.string().email({
-    minDomainSegments: 2,
-    tlds: { allow: ["com", "cu"] },
-  }),
-  password: Joi.string().pattern(new RegExp(decimal)),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "cu"] },
+    })
+    .required(),
+  password: Joi.string().pattern(new RegExp(decimal)).required(),
   repeatPassword: Joi.ref("password"),
   access_token: [Joi.string(), Joi.number()],
 })
