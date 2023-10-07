7e1dbe5c4cd042333ebb5213fbdf82e56cbd033d
Randy <93347169+randy930529@users.noreply.github.com>
2023-10-07 05:23:15 +0000

    delete hook useSendMail

--- src/auth/hooks/useSendMail.ts
+++
@@ -1,9 +0,0 @@
-//import { transporter } from "../../utils/emailService";
-
-export async function useSendMail(mail) {
-  try {
-    //await transporter.sendMail(mail);
-  } catch (error) {
-    throw new Error(error.message);
-  }
-}
