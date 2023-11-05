e0a9bf81301b49d309e4b661fd3a212bf4ffe666
Randy <93347169+randy930529@users.noreply.github.com>
2023-11-05 09:17:47 +0000

    create to api home page

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { AppDataSource } from "./data-source";
 import { Routes } from "./router";
 import { errorHandler } from "./errors/middlewares/errorHandler";
 import { appConfig } from "../config";
+import * as pug from "pug";
 
 //Para provar el envio de correo. Eliminar!
 import * as nodemailer from "nodemailer";
@@ -54,6 +55,13 @@ AppDataSource.initialize()
     app.use(errorHandler);
 
     // setup express app here
+    app.get("/", function (req: Request, res: Response, next: Function) {
+      const html = pug.renderFile(`${__dirname}/views/api/index.pug`, {
+        title: "API-rest dj",
+        url: "logo.empresa",
+      });
+      res.render(html);
+    });
 
     // TEMPLATE ENGINE
     app.set("view engine", "pug");
