a800632bdf83925ed5d5d91112ca9070ad22a17a
randy930529 <randy930529@gmail.com>
2024-04-05 15:01:55 -0400

    Node-fetch: Disable SSL verification

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -1,8 +1,16 @@
 import fetch from "node-fetch";
+import * as https from "https";
+import { ENV } from "./settings/environment";
 
 export default async function get(
   path: URL,
   config?: { method: string; [key: string]: unknown }
 ) {
+  const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+  });
+
+  if (ENV.debug !== "development") config.agent = httpsAgent;
+
   return await fetch(path, config);
 }
