e4c3204e25caa4219e3750477443ac54c9961916
randy930529 <randy930529@gmail.com>
2024-05-10 04:03:51 -0400

    upgrade create option where to the query param

--- src/base/utils/createFindOptions.ts
+++ src/base/utils/createFindOptions.ts
@@ -1,6 +1,6 @@
 import { Request } from "express";
 
-export function createFindOptions(req: Request, otherOptions: any = {}) {
+export function createFindOptions<T>(req: Request, otherOptions?): T {
   const optionsString = req.query.options as string;
   return {
     ...(optionsString && JSON.parse(optionsString)),
