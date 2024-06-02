2c40177d95f72c543184755898bee96dd08f8be3
randy930529 <randy930529@gmail.com>
2024-06-01 23:29:17 -0400

    upgrade Element to check duplicate element

--- src/entity/Element.ts
+++ src/entity/Element.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, JoinColumn, ManyToOne, OneToMany } from "typeorm";
+import {
+  Entity,
+  Column,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  BeforeInsert,
+} from "typeorm";
 import Model from "./Base";
 import { Profile } from "./Profile";
 import { Account } from "./Account";
@@ -34,4 +41,37 @@ export class Element extends Model {
     (supportDocument) => supportDocument.element
   )
   supportDocuments: SupportDocument[];
+
+  @BeforeInsert()
+  async checkDuplicateElementForProfile(): Promise<void> {
+    const elementForProfileWithSameName = await Element.findOne({
+      where: {
+        profile: { id: this.profile?.id },
+        description: this.description,
+        type: this.type,
+      },
+    });
+
+    if (
+      elementForProfileWithSameName &&
+      this.id != elementForProfileWithSameName.id
+    ) {
+      throw new Error("Only a element with the same name are allowed.");
+    }
+
+    if ((this.type == "g" && this.group == "pd") || this.group == "dd") {
+      const allowedCount = this.group == "pd" ? 6 : 2;
+      const countElementsForProfilePD = await Element.count({
+        where: {
+          profile: { id: this.profile?.id },
+          type: this.type,
+          group: this.group,
+        },
+      });
+
+      if (countElementsForProfilePD >= allowedCount) {
+        throw new Error(`Only ${allowedCount} possible elements to deduce are allowed.`);
+      }
+    }
+  }
 }
