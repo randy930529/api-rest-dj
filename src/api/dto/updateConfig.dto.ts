ef0bd8b91d1cc36a6008e16033fc4527f0bf31b8
randy930529 <randy930529@gmail.com>
2024-11-15 12:04:53 -0500

    update manager api config

--- src/api/dto/updateConfig.dto.ts
+++ src/api/dto/updateConfig.dto.ts
@@ -1,19 +1,5 @@
-import { Config } from "../../entity/Config";
+import { CreateConfigDTO } from "./createConfig.dto";
 
-export type UpdateConfigDTO = {
+export type UpdateConfigDTO = CreateConfigDTO & {
   id: number;
-  email_from?: string;
-  license_free_days?: number;
-  business_name?: string;
-  business_source?: number;
-  business_address?: string;
-  business_phone?: string;
-  business_email?: string;
-  MEa_By_MFP?: number;
-  PPD_PERCENTAGE?: number;
-  PE_0_10000?: number;
-  PE_10000_20000?: number;
-  PE_20000_30000?: number;
-  PE_30000_50000?: number;
-  PE_ABOVE_50000?: number;
 };
--- src/entity/Config.ts
+++ src/entity/Config.ts
@@ -1,7 +1,27 @@
-import { Column, Entity } from "typeorm";
+import {
+  BeforeInsert,
+  BeforeRemove,
+  BeforeUpdate,
+  Column,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  OneToOne,
+} from "typeorm";
 import Model from "./Base";
 import { appConfig } from "../../config";
 import { ColumnNumericTransformer } from "../utils/ColumnNumericTransformer";
+import { Account } from "./Account";
+import { ProgressiveScale } from "./ProgressiveScale";
+import { User, UserRole } from "./User";
+
+const defaultCompany = JSON.stringify({
+  name: "",
+  source: null,
+  address: "",
+  phoneNumber: "",
+  email: "",
+});
 
 @Entity()
 export class Config extends Model {
@@ -11,81 +31,76 @@ export class Config extends Model {
   @Column({ default: appConfig.licenseFreeDays ?? 0 })
   license_free_days: number;
 
-  @Column({ default: appConfig.businessMetadata.name })
-  business_name: string;
-
-  @Column({ default: appConfig.businessMetadata.source })
-  business_source: number;
-
-  @Column({ default: appConfig.businessMetadata.address })
-  business_address: string;
-
-  @Column({ default: appConfig.businessMetadata.phone })
-  business_phone: string;
-
-  @Column({ default: appConfig.businessMetadata.email })
-  business_email: string;
+  @Column({ type: "integer", width: 4, nullable: true })
+  inactivity_max: number;
 
   @Column({
     type: "numeric",
     precision: 19,
     scale: 2,
     transformer: new ColumnNumericTransformer(),
-    default: appConfig.accountingConstants.MEa_By_MFP ?? 0,
+    default: 0,
   })
-  MEa_By_MFP: number;
+  except_min: number;
+
+  @Column({ type: "integer", width: 4, nullable: true })
+  allowance: number;
 
   @Column({
-    type: "numeric",
-    precision: 3,
-    scale: 2,
-    transformer: new ColumnNumericTransformer(),
-    default: appConfig.accountingConstants.PPD_PERCENTAGE ?? 1,
+    type: "json",
+    default: defaultCompany,
   })
-  PPD_PERCENTAGE: number;
+  company: string;
 
   @Column({
-    type: "numeric",
-    precision: 3,
-    scale: 2,
-    transformer: new ColumnNumericTransformer(),
-    default: appConfig.accountingConstants.PE_0_10000 ?? 1,
+    type: "json",
+    nullable: true,
   })
-  PE_0_10000: number;
+  mora_scale: string;
 
-  @Column({
-    type: "numeric",
-    precision: 3,
-    scale: 2,
-    transformer: new ColumnNumericTransformer(),
-    default: appConfig.accountingConstants.PE_10000_20000 ?? 1,
-  })
-  PE_10000_20000: number;
+  @OneToOne(() => Account, { nullable: true })
+  @JoinColumn()
+  accountBox: Account;
 
-  @Column({
-    type: "numeric",
-    precision: 3,
-    scale: 2,
-    transformer: new ColumnNumericTransformer(),
-    default: appConfig.accountingConstants.PE_20000_30000 ?? 1,
-  })
-  PE_20000_30000: number;
+  @OneToOne(() => Account, { nullable: true })
+  @JoinColumn()
+  accountBank: Account;
 
-  @Column({
-    type: "numeric",
-    precision: 3,
-    scale: 2,
-    transformer: new ColumnNumericTransformer(),
-    default: appConfig.accountingConstants.PE_30000_50000 ?? 1,
-  })
-  PE_30000_50000: number;
+  @OneToMany(
+    () => ProgressiveScale,
+    (progressiveScale) => progressiveScale.config,
+    { cascade: ["insert", "update"] }
+  )
+  progressiveScale: ProgressiveScale[];
 
-  @Column({
-    type: "numeric",
-    precision: 3,
-    scale: 2,
-    transformer: new ColumnNumericTransformer(),
-    default: appConfig.accountingConstants.PE_ABOVE_50000 ?? 1,
-  })
-  PE_ABOVE_50000: number;
+  @OneToOne(() => Account, { nullable: true })
+  @JoinColumn()
+  accountLongTerm: Account;
+
+  @OneToOne(() => Account, { nullable: true })
+  @JoinColumn()
+  accountShortTerm: Account;
+
+  @OneToOne(() => User, { nullable: true })
+  @JoinColumn()
+  user: User;
+
+  toJSON() {
+    return {
+      ...this,
+      company: JSON.parse(this.company),
+      mora_scale: JSON.parse(this.mora_scale),
+      created_at: undefined,
+      updated_at: undefined,
+    };
+  }
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  @BeforeRemove()
+  checkIsUserAdmin(): void {
+    if (this.user && this.user.role !== UserRole.ADMIN) {
+      throw new Error("User does not have permission to perform this action.");
+    }
+  }
 }
